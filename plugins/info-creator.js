40a6f06871850e25256218d0b570f6a9ac882790
SenRyhn <reyhanluthers@gmail.com>
2023-10-26 08:25:55 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,8 +1,8 @@
 function handler(m) {
   
   const kontak = {
-	"displayName": 'My owner',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN: VynaaChan\nitem1.TEL;waid=6289518965891:6289518965891\nitem1.X-ABLabel:\nJangan Spam Kak\nURL;Email Owner:vynaachan@gmail.com\nORG: INI OWNER\nEND:VCARD`
+	"displayName": 'SenR',
+	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN: SenR\nitem1.TEL;waid=628975626924:628975626924\nitem1.X-ABLabel:\nJangan Spam Kak\nURL;Email Owner:reyhanluthers9@gmail.com\nORG: INI OWNER\nEND:VCARD`
 }
 
 conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
